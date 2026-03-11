4e5c6f6250d9b7e342181455ae05fb8dc91109aa
jideatom <95307473+jideatom@users.noreply.github.com>
2026-03-11 17:26:04 +0100

    Add files via upload

---
+++ sw-6.js
@@ -0,0 +1,4 @@
+const C='ai-net-v2';const A=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
